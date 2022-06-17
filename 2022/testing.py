615bdb7ebf913922ebf2456d0161592c193387c0
zxzimeng <zxzimeng@gmail.com>
2022-06-17 13:15:53 -0700

    revert back

--- 2022/testing.py
+++
@@ -1,4 +0,0 @@
-def make_incrementor(n):
-    return lambda x: x + n
-
-f =  make_incrementor(42)
---
+++ p202happynumber_test.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        
+def test():
+    solution = Solution()
+    assert inc(3) == 5
