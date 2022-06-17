c56ca712c17c07e2d2c6b6ecfb40869549fe7e47
zxzimeng <zxzimeng@gmail.com>
2022-06-17 13:28:14 -0700

    p202

--- p202happynumber_test.py
+++ p202happynumber_test.py
@@ -1,6 +1,28 @@
 class Solution:
-    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+    def getNewNumber(self, n: int):
+        newNumber = 0
+        for x in list(str(n)):
+            newNumber += int(x)**2
+        return newNumber
+
+    def isHappy(self, n: int) -> bool:
+        alreadySeen = [n]
         
-def test():
-    solution = Solution()
-    assert inc(3) == 5
+        newNum = self.getNewNumber(n)
+        if newNum == 1:
+            return True
+
+        while newNum not in alreadySeen:
+            if newNum == 1:
+                return True
+            alreadySeen.append(newNum)
+            newNum = self.getNewNumber(newNum)
+
+        return False
+        
+        
+def test_():
+    testfunc = Solution().isHappy
+    assert testfunc(2) == False
+    assert testfunc(19) == True
+    assert testfunc(1) == True
