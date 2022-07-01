9da454f8433074a880d0a72b4c58b9e5a94e4051
zx4017555 <zx4017555@gmail.com>
2022-07-01 06:48:41 -0700

    1903 solved

---
+++ 1903largestOdd.py
@@ -0,0 +1,14 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for i, x in enumerate(list(num[::-1])):
+            print(i, x, num, list(num[::-1]))
+            if int(x) % 2 == 1:
+                return num[0:(len(num)-1)-i+1]
+
+        return ""
+
+def test_():
+    testfunc = Solution().largestOddNumber
+    assert testfunc("52") == "5"
+    assert testfunc("4206") == ""
+    assert testfunc("35427") == "35427"
