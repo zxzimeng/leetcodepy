f3a3d58d7aed43e08a2534ce5837772da811dbb4
zxzimeng <zxzimeng@gmail.com>
2023-01-18 10:04:15 -0800

    progress on merging accounts

--- 721accountsmerge_test.py
+++ 721accountsmerge_test.py
@@ -1,16 +1,25 @@
+#Given a list of accounts where each element accounts[i] is a list of strings, 
+#where the first element accounts[i][0] is a name, 
+#and the rest of the elements are emails representing emails of the account.
+
+from typing import List
+
 class Solution:
 
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        pass
+        for account in accounts:
+            emails = []
+            name = ''
+            for email in accounts
+
+
+
 
 
 def test_():
     testfunc = Solution().accountsMerge
-    assert testfunc([["John", "johnsmith@mail.com", "john_newyork@mail.com"],
-                     ["John", "johnsmith@mail.com", "john00@mail.com"],
-                     ["Mary", "mary@mail.com"],
-                     ["John", "johnnybravo@mail.com"]]) == [[
-                         "John", "john00@mail.com", "john_newyork@mail.com",
-                         "johnsmith@mail.com"
-                     ], ["Mary",
-                         "mary@mail.com"], ["John", "johnnybravo@mail.com"]]
+    assert testfunc([["John", "johnsmith@mail.com", "john_newyork@mail.com"]["John", "johnsmith@mail.com",
+                                                                             "john00@mail.com"],
+                     ["Mary", "mary@mail.com"], ["John", "johnnybravo@mail.com"]
+                    ]) == [["John", "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com"],
+                           ["Mary", "mary@mail.com"], ["John", "johnnybravo@mail.com"]]
